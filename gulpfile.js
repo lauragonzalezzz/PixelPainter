e4e9820bb82da7edcdc613a67de3c97afca8c5fa
Laura <lg3295@gmail.com>
2016-05-03 19:34:26 -1000

    adds gulp to compile sass

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var nodemon = require('gulp-nodemon');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+  gulp.watch("scss/**/*.scss", ['sass']);
+});
+
+gulp.task('start', function () {
+  nodemon({
+    script : 'server.js',
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("./public/css"))
+});
+
+gulp.task('default', ['serve', 'start']);
