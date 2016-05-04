8e4fb35654f0df7ca87e0449645d94b92541d486
Laura <lg3295@gmail.com>
2016-05-03 15:46:26 -1000

    makes xhr request to sent pixel statuses to correct route

---
+++ public/js/save.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const saveBtn = document.getElementById('saveBtn');
+saveBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  let name = document.getElementById('name').value;
+  const pixels = document.querySelectorAll('.allDivs');
+  const pixelStates = [];
+  for (let i = 0; i < pixels.length; i++) {
+    if (!pixels[i].style.background){
+      pixelStates.push("empty");
+    }
+    else {
+      pixelStates.push(pixels[i].style.background);
+    }
+  }
+
+  const payload = "name=" + name + "&pixelStates=" + pixelStates;
+
+  const xReq = new XMLHttpRequest();
+  xReq.addEventListener('load', (event) => {});
+  xReq.open("POST", '/save');
+  xReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xReq.send(payload);
+
+});
