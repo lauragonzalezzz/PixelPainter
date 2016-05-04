9aea7402a4279171dc6dac92eed241e53bb49867
Laura <lg3295@gmail.com>
2016-05-03 15:45:16 -1000

    adds route that saves client picture to pixel_painter db

--- server.js
+++ server.js
@@ -5,33 +5,47 @@ const app = express();
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 const Schema = mongoose.Schema;
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.get('/',)
+// app.get('/', (req, res) => {
+// });
 
-app.post('/save', (req, res) => {
 
-});
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Database connected!');
-  const pixelPainterSchema = new Schema({
-    username : {type : String, required : true, unique : true },
-    url : {type : String, required : true}
-    title : {type : String, required : true},
-    pixelStates : [],
-    createdAt : Date,
-    updatedAt : Date
-  });
-
-  const Masterpiece = mongoose.model('Masterpiece', pixelPainterSchema);
 
 });
 
 mongoose.connect('mongodb://localhost/pixel_painter');
+const pixelPainterSchema = new Schema({
+  name : String,
+  pixelStates : [],
+  createdAt : Date,
+  updatedAt : Date
+});
+
+const Masterpiece = mongoose.model('Masterpiece', pixelPainterSchema);
+
+
+app.post('/save', (req, res) => {
+  const image = new Masterpiece({
+    name : req.body.name,
+    pixelStates : req.body.pixelStates
+  })
+  image.save((err, image) => {
+    if (err) {
+      return console.error(err);
+    };
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
